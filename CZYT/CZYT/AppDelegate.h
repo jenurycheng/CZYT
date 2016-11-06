8a8cf34107265a9c58ece24984764eb28f95e051
chengchao <413421780@qq.com>
2016-11-06 13:34:18 +0800

    first

---
+++ CZYT/CZYT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZYT
+//
+//  Created by jerry cheng on 2016/11/6.
+//  Copyright © 2016年 Chester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZYT/CZYT/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CZYT
+//
+//  Created by jerry cheng on 2016/11/6.
+//  Copyright © 2016年 Chester. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
